6e16c2866ccc95fd76ca858695016284591b5ae1
Phani Saripalli <saripalli.phani@gmail.com>
2020-07-08 21:46:28 +0200

    eyz-286 refactored inapp functions

--- android/library/src/main/java/org/weex/plugin/weexplugininapp/WeexPluginInappModule.java
+++ android/library/src/main/java/org/weex/plugin/weexplugininapp/WeexPluginInappModule.java
@@ -7,15 +7,16 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.alibaba.weex.plugin.annotation.WeexModule;
+import com.android.billingclient.api.BillingClient;
 import com.taobao.weex.annotation.JSMethod;
 import com.taobao.weex.bridge.JSCallback;
 import com.taobao.weex.common.WXModule;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.weex.plugin.weexplugininapp.billing.BillingHandler;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 @WeexModule(name = "weexPluginInapp")
@@ -26,7 +27,6 @@ public class WeexPluginInappModule extends WXModule {
     // Define the callback
     private JSCallback jsCallback;
     private Activity thisActivity;
-    private String productId;
 
     // Inapp repsonse map
     private Map<String, Object> response = new HashMap();
@@ -61,56 +61,62 @@ public class WeexPluginInappModule extends WXModule {
         return data;
     }
 
-    @JSMethod(uiThread = true)
-    public void show(String params, String key) throws JSONException {
-        Log.d(TAG, "-> Showing!!!");
-        String data = this.getDataByKey(params, key);
-        if (data != null) {
-            // TODO
-        } else {
-            Toast.makeText(mWXSDKInstance.getContext(), data, Toast.LENGTH_SHORT).show();
+    private String getDataFromJSONString(String message) {
+        StringBuilder data = new StringBuilder();
+        JSONObject json = new JSONObject();
+
+        try {
+            json = new JSONObject(message);
+
+            Iterator<String> it = json.keys();
+
+            while (it.hasNext()) {
+                String key = it.next();
+                data.append(key).append(": ").append(json.get(key));
+            }
+        } catch (Throwable t) {
+            data = new StringBuilder();
+            Log.e("-> show", "Could not parse malformed JSON: \"" + json + "\"");
         }
+
+        return data.toString();
     }
 
-    @JSMethod(uiThread = true)
+    @JSMethod()
+    public void show(String message) {
+        Log.d(TAG, "-> Showing!!!");
+        String data = this.getDataFromJSONString(message);
+        Toast.makeText(mWXSDKInstance.getContext(), data, Toast.LENGTH_SHORT).show();
+    }
+
+    @JSMethod()
     public void buy(String productId, JSCallback jsCallback) {
         Log.d(TAG, "-> buy " + productId);
-
-        Toast.makeText(mWXSDKInstance.getContext(),
-                "Buy ", Toast.LENGTH_SHORT
-        ).show();
 
         this.jsCallback = jsCallback;
         this.response.put("productId", productId);
         this.thisActivity = ((Activity) mWXSDKInstance.getContext());
-        this.productId = productId;
 
         Log.d("productId", this.response.toString());
 
-        this.doPurchase();
+        BillingHandler bh = new BillingHandler(thisActivity, productId, BillingClient.SkuType.INAPP, this.jsCallback);
+        bh.startPurchaseFlow();
     }
 
-    @JSMethod(uiThread = true)
+    @JSMethod()
     public void subscribe(String productId, JSCallback jsCallback) {
         Log.d(TAG, "-> Subscribe " + productId);
-
-        Toast.makeText(mWXSDKInstance.getContext(),
-                "subscribe ", Toast.LENGTH_SHORT
-        ).show();
 
         this.jsCallback = jsCallback;
         this.thisActivity = ((Activity) mWXSDKInstance.getContext());
 
-        // this.doPurchase();
+        BillingHandler bh = new BillingHandler(thisActivity, productId, BillingClient.SkuType.SUBS, this.jsCallback);
+        bh.startPurchaseFlow();
     }
 
     @JSMethod(uiThread = true)
     public void manageSubscriptions() {
         Log.d(TAG, "-> manageSubscriptions");
-
-        Toast.makeText(mWXSDKInstance.getContext(),
-                "manageSubscriptions", Toast.LENGTH_SHORT
-        ).show();
 
         this.thisActivity = ((Activity) mWXSDKInstance.getContext());
         Uri webpage = Uri.parse("http://play.google.com/store/account/subscriptions");
@@ -139,10 +145,5 @@ public class WeexPluginInappModule extends WXModule {
             // jsCallback('getProductInfo');
             // this.doPurchase();
         }
-    }
-
-    private void doPurchase() {
-        BillingHandler bh = new BillingHandler(thisActivity, productId, this.jsCallback);
-        bh.init();
     }
 }
